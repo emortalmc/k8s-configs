de4abb4337cb1ee856f389995883cfdd8304b92f
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-01-24 19:20:40 +0000

    Update configs

--- agones/initAgones.sh
+++ agones/initAgones.sh
@@ -2,11 +2,13 @@ kubectl create namespace emortalmc
 helm repo add agones https://agones.dev/chart/stable
 helm repo update agones
 helm install agones agones/agones --set "gameservers.namespaces={emortalmc}" \
-  --set "agones.allocator.service.grpc.enabled=false" \
+  --set "agones.allocator.service.http.enabled=false" \
   --set "agones.metrics.prometheusEnabled=true" \
   --set "agones.metrics.prometheusServiceDiscovery=true" \
+  --set "agones.metrics.serviceMonitor.enabled=true" \
   --set "agones.allocator.serviceMetrics.http.portName=metrics" \
   --set "agones.featureGates=PlayerTracking=true&PlayerAllocationFilter=true" \
+  --set "agones.controller.allocationBatchWaitTime=300ms" \
   -n agones-system --create-namespace
 
 FORWARDING_TOKEN=$(cat /dev/urandom | fold -w 128 | head -n 1)
