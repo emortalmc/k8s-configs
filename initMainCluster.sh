a107986c4b9085cdb94f896a89799a9fb9e42082
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-06-13 19:11:25 +0100

    Add stuff

--- initMainCluster.sh
+++ initMainCluster.sh
@@ -1,2 +1,9 @@
 TOKEN=$(head -c 1000 /dev/urandom | tr -dc 'a-zA-Z0-9~@^&*_-' | fold -w 128 | head -n 1)
-curl -sfL https://get.k3s.io | K3S_TOKEN="$TOKEN" sh -s - server
+
+NODE_NAME=emc-01
+ADVERTISE_ADDRESS=65.108.100.154
+NODE_IP=65.108.100.154
+curl -sfL https://get.k3s.io | K3S_TOKEN="$TOKEN" sh -s - server --flannel-backend=wireguard-native \
+  --advertise-address "$ADVERTISE_ADDRESS" \
+  --node-name "$NODE_NAME" \
+  --node-ip "$NODE_IP"
