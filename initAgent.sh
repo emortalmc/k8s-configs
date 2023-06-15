beb9862660c2465b6b7ca19d0aa5e5bdde30a47a
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-06-15 18:15:03 +0100

    Generic updates

--- initAgent.sh
+++ initAgent.sh
@@ -1,2 +1,3 @@
 TOKEN=""
-curl -sfL https://get.k3s.io | K3S_URL=https://65.108.100.154:6443 K3S_TOKEN="$TOKEN" sh -s -
+curl -sfL https://get.k3s.io | K3S_URL=https://65.108.100.154:6443 K3S_TOKEN="$TOKEN" sh -s - \
+  --resolv-conf /run/systemd/resolve/resolv.conf
--- initMainCluster.sh
+++ initMainCluster.sh
@@ -6,4 +6,5 @@ NODE_IP=65.108.100.154
 curl -sfL https://get.k3s.io | K3S_TOKEN="$TOKEN" sh -s - server --flannel-backend=wireguard-native \
   --advertise-address "$ADVERTISE_ADDRESS" \
   --node-name "$NODE_NAME" \
-  --node-ip "$NODE_IP"
+  --node-ip "$NODE_IP" \
+  --resolv-conf /run/systemd/resolve/resolv.conf # All systems are installed with Ubuntu so this is necessary
