da9455e1cf299cb49562fdbbb0f4fd36eae844ff
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-06-15 11:30:34 +0100

    Added DNS configs for DNSControl and updated ingresses

---
+++ dns/dnsconfig.js
@@ -0,0 +1,34 @@
+// @ts-check
+// <reference path="types-dnscontrol.d.ts" />
+
+var REG_NONE = NewRegistrar('none');
+var DNS_POWERDNS = NewDnsProvider('powerdns');
+
+/**
+ * Production zone
+ */
+
+D("emc", REG_NONE, DnsProvider(DNS_POWERDNS),
+    // Root info
+    A("@", "100.64.209.7"),
+
+    // Service CNAMEs
+    CNAME("argo", "emc."),
+    CNAME("metrics", "emc."),
+    CNAME("linkerd", "emc."),
+    CNAME("traefik", "emc.")
+);
+
+/**
+ * Staging zone
+ */
+
+D("emc.staging", REG_NONE, DnsProvider(DNS_POWERDNS),
+    // Root info
+    A("@", "100.113.150.36"),
+
+    CNAME("argo", "emc.staging."),
+    CNAME("metrics", "emc.staging."),
+    CNAME("linkerd", "emc.staging."),
+    CNAME("traefik", "emc.staging.")
+);
