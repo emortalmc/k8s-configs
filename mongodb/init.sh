dbfeb46e279c547ff713ece89d6b0c8316588b79
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-09-22 16:45:32 +0100

    Specify MongoDB chart version

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -2,5 +2,6 @@ helm repo add mongodb https://mongodb.github.io/helm-charts
 
 kubectl create namespace mongodb
 helm install mongodb-operator mongodb/community-operator \
+  --version 0.8.0 \
   --values values.yaml \
   -n mongodb
