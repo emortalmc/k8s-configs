1edd46c02da325c3c7bd7b6d0fbaa6baa58d41cb
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-09-25 23:13:01 +0100

    Upgrade prom stack, rework mc grafana dashboard

--- monitoring/prom-stack/install-metrics.sh
+++ monitoring/prom-stack/install-metrics.sh
@@ -6,5 +6,5 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update prometheus-community
 
 helm install prom-stack prometheus-community/kube-prometheus-stack \
-  --values ./values.yaml --version 50.3.1 \
+  --values ./values.yaml --version 51.2.0 \
   -n monitoring
--- monitoring/prom-stack/upgrade-metrics.sh
+++ monitoring/prom-stack/upgrade-metrics.sh
@@ -1,3 +1,3 @@
 helm upgrade prom-stack prometheus-community/kube-prometheus-stack \
-  --values values.yaml --version 50.3.1 \
+  --values values.yaml --version 51.2.0 \
   -n monitoring
